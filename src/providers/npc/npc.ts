40d6a2176abdd89e61a4bbdba6df4b8513f0acbe
Felipe Menezes <felm84@hotmail.com>
2018-04-05 18:30:56 +1000

    Added speeches[] to NpcProvider.

--- src/providers/npc/npc.ts
+++ src/providers/npc/npc.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { DataProvider } from '../data/data';
 import { ICharacter } from '../interface/character';
+import { ISpeech } from '../interface/speech';
 
 @Injectable()
 export class NpcProvider {
   
   public npc: ICharacter;
+  public speeches: ISpeech[] = [];
 
   constructor(private data: DataProvider) {
     console.log('NpcProvider');
@@ -14,6 +16,17 @@ export class NpcProvider {
   setNpc(npcId) {
     this.npc = this.data.charactersArray[this.findNpc(npcId)];
     console.log('New Npc - ' + this.npc.name );
+  }
+
+  feedSpeeches() {
+    for (const speech of this.npc.speeches) {
+      this.speeches.push(this.data.speechesArray[this.findSpeech(speech)]);
+    }
+  }
+
+  findSpeech(id) {
+    let index = this.data.speechesArray.findIndex((element) => element.id === id);
+    return index;
   }
   
   findNpc(id) {
