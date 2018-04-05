e5bf17010adeadd319761ff7959735800dafa049
Felipe Menezes <felm84@hotmail.com>
2018-04-05 18:31:36 +1000

    Added items[] to LocationProvider.

--- src/providers/location/location.ts
+++ src/providers/location/location.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
+
 import { ILocation } from '../interface/location';
+import { IItem } from '../interface/item';
+import { DataProvider } from '../data/data';
 
 @Injectable()
 export class LocationProvider {
   
   public location: ILocation;
 
-  constructor() {
+  public items: IItem[] = [];
+
+  constructor(private data: DataProvider) {
     console.log('LocationProvider');
   }
 
@@ -18,6 +23,18 @@ export class LocationProvider {
   setLocation(location) {
     this.location = location
     console.log('New location - ' + this.location.name );
+  }
+
+  findIndex(id) {
+    let index = this.data.itemsArray.findIndex((element) => element.id === id);
+    return index;
+  }
+
+  feedList() {
+    for (const item of this.location.items) {
+      this.items.push(this.data.itemsArray[this.findIndex(item)]);
+      console.log(this.data.itemsArray[this.findIndex(item)]);
+    }
   }
 
   releaseItem() {
