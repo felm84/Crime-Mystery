a4853a131c25dfd7d8979e2c939ccb1da0721828
Felipe Menezes <felm84@hotmail.com>
2018-04-23 23:16:56 +1000

    Added more paths and updated current_location.

--- src/pages/current-location/current-location.ts
+++ src/pages/current-location/current-location.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { NavParams, PopoverController, ViewController, LoadingController } from 'ionic-angular';
 import { GameProvider } from '../../providers/game/game';
+import { DataProvider } from '../../providers/data/data';
 
 // $ declared to be used as jquery
 declare var $: any;
 @Component({
   template: `<ion-list>
-      <button ion-item icon-lef (click)="chat()">
-        <ion-icon name="chatbubbles"></ion-icon>
-        Chat
-      </button>
-
       <button ion-item icon-lef (click)="getWarrant()">
         <ion-icon name="document"></ion-icon>
         Get Warrant
@@ -31,22 +27,25 @@ declare var $: any;
 
 export class PlusMenu {
 
+  private _npc = this._game.npcProvider;
+
   constructor(
     public viewCtrl: ViewController,
     public loadingCtrl: LoadingController,
+    private _data: DataProvider,
     private _game: GameProvider
   ) {};
 
-  chat() {
-    this.close();
-  }
-
   getWarrant() {
-    if (!this._game.itemProvider.warrantInProcess) {
+    if (!this._game.itemProvider.warrantInProcess &&
+      this._npc.canSearch) {
       let warrant = this._game.itemProvider.findItem(100);
       this._game.itemProvider.presentAlert('Search warrant', 'It will take up to 30 minutes to be done.');
       this._game.itemProvider.analyseItem(warrant);
       this._game.itemProvider.warrantInProcess = true;
+    } else if (!this._npc.canSearch) {
+      this._game.itemProvider.presentAlert('Search Warrant', `Have a chat with the locals first and see how
+      they can help you. Please, tap on the <b>character</b> to start a chat, then on yours.`);
     } else {
       this._game.itemProvider.presentAlert('Search warrant', 'You already have one in process, check you bag of items.');
     }
@@ -55,8 +54,15 @@ export class PlusMenu {
 
   //Working
   searchArea() {
-    if (!this._game.playerProvider.currentLocation.require_warrant || this._game.playerProvider.hasWarrant) {
-      if (this._game.playerProvider.hasWarrant) this._game.playerProvider.hasWarrant = false;
+    if ((!this._game.playerProvider.currentLocation.require_warrant || 
+      this._game.playerProvider.hasWarrant) && this._npc.canSearch) {
+      if (this._game.playerProvider.currentLocation.require_warrant && 
+        this._game.playerProvider.hasWarrant) {
+        this._game.playerProvider.hasWarrant = false;
+      }
+      this._data.locationsArray[
+        this._data.locationsArray.findIndex(x => x.id === this._game.locationProvider.location.id)
+      ].require_warrant = false;
       let items = this._game.locationProvider.releaseItems();
       let loading = this.loadingCtrl.create({
         content: 'Searching area...',
@@ -67,8 +73,11 @@ export class PlusMenu {
         this._game.itemProvider.addItemsToColletion(items);
       });
       loading.present();      
+    } else if (!this._npc.canSearch) {
+      this._game.itemProvider.presentAlert('Search Area', `Have a chat with the locals first and see how
+      they can help you. Please, tap on the <b>character</b> to start a chat, then on yours.`);
     } else {
-      this._game.itemProvider.presentAlert('Search Warrant', `Search warrant is required for this location. 
+      this._game.itemProvider.presentAlert('Search Area', `Search warrant is required for this location. 
       Please, select <b>Get Warrant</b> in the header menu.`);
     }
     this.close();
@@ -87,8 +96,14 @@ export class PlusMenu {
 export class CurrentLocationPage {
 
   //#region PROPERTIES
-  // speak works as a switch
-  private speak: boolean = false;
+  private _location = this._game.locationProvider;
+  private _npc = this._game.npcProvider;
+  private _player = this._game.playerProvider;
+
+  // speak works as a switch (false = npc speaks, true = player speaks)
+  private _speak: boolean = false;
+  private _playerPhrase = this._player.currentSpeech;
+  private _npcPhrase = this._npc.currentSpeech;
   //#endregion
 
   constructor( 
@@ -98,7 +113,7 @@ export class CurrentLocationPage {
   ) {};
 
   ionViewDidLeave(){
-    this.speak = false;
+    this._speak = false;
     this.clearChat();
   }
   
@@ -116,34 +131,48 @@ export class CurrentLocationPage {
    Shows the npc or player conversation accordingly to
    the element and speak property value  */
   showSpeech(element) {
-    if (this.speak === false && element === 'npc') {
-      this.speak = true;
+    if (this._speak === false && element === 'npc') {
+      this.npcTalk();
+    } else if (this._speak && element === 'player') {
+      this.playerTalk();
+    }    
+  }
 
-      if (!this._game.npcProvider.greeted) {
-        this._game.npcProvider.currentSpeech = this._game.npcProvider.greetPlayer();
-        this._game.npcProvider.greeted = true;
-      } else {
-        this._game.npcProvider.answerPlayer(this._game.playerProvider.currentSpeech);
-      }
-      
-      $('#playerChat').fadeOut();
-      $('#npcChat').fadeIn();
-      // Push npc currentSpeech into its history[]
-      this._game.npcProvider.npc.history.push(
-        this._game.npcProvider.npc.name + " - " + this._game.npcProvider.currentSpeech.phrase);
+  npcTalk() {
+    if (!this._npc.greeted) {
+      this._npcPhrase = this._npc.greetPlayer();
+      this._npc.greeted = true;
+    } else {      
+      this._npcPhrase = this._npc.answer(this._player, this._location.location);
+    }
+    
+    $('#playerChat').fadeOut();
+    $('#npcChat').fadeIn();
+    // Push npc currentSpeech into its history[]
+    this._npc.npc.history.push(
+      this._npc.npc.name + " - " + this._npc.currentSpeech.phrase);
 
-    } else if (this.speak && element === 'player') {
-      this.speak = false;
+    this._speak = true;
+  }
 
-      this._game.playerProvider.answerNpc(this._game.npcProvider.currentSpeech);
+  playerTalk() {
+    if (this._npc.currentSpeech.id === 74) {
+      this._game.itemProvider.presentAlert('Search Warrant', `Search warrant is required for this location. 
+    Please, select <b>Get Warrant</b> in the header menu.`);
+    } else if (this._npc.currentSpeech.id === 64) {
+      this._game.itemProvider.presentAlert('Search Area', `Now you may search the area by selecting <b>search 
+      area</b> in the header menu.`);
+    } else {
+      this._playerPhrase = this._game.playerProvider.answer(this._npc, this._location.location);
 
       $('#npcChat').fadeOut();
       $('#playerChat').fadeIn();
       
       // Push player currentSpeech into current npcs history[]
-      this._game.npcProvider.npc.history.push(
+      this._npc.npc.history.push(
         this._game.playerProvider.player.name + " - " + this._game.playerProvider.currentSpeech.phrase);
-    }    
+    }
+    this._speak = false;
   }
 
   /* clearChat() method
