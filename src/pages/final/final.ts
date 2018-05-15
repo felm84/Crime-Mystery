2577dd11f48cf66ded3a6845e99c4eec3548de0b
Felipe Menezes <felm84@hotmail.com>
2018-05-15 17:01:40 +1000

    FinalPage fully implemented.

--- src/pages/final/final.ts
+++ src/pages/final/final.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Platform, App } from 'ionic-angular';
+import { SaveProvider } from '../../providers/save/save';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-final',
@@ -7,11 +9,59 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class FinalPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private _criminal: boolean;
+  private _config = {
+    title: 'GAME OVER',
+    phrase: 'You have arrested the wrong person, and the real criminal got away!'
+  }
+
+  /**
+   * FinalPage constructor
+   * @param appCtrl type from App
+   * @param platform type from Plarform
+   * @param navCtrl type from NavController
+   * @param navParams type from NavParams
+   * @param _save type from SaveProvider
+   */
+  constructor(
+    public appCtrl: App,
+    public platform: Platform,
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private _save: SaveProvider
+  ) {
+    //this.appCtrl.getRootNavById('n4').getAllChildNavs()[0].parent.popToRoot();
+    this._criminal = this.navParams.get('criminal');
+    this.applyConfig();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FinalPage');
+    console.log(`Final - ${this.navCtrl}`);
+    this._save.storage.clear();
   }
 
+  /**
+   * applyConfig() method
+   * Apply final presentation accordingly to _criminal property result
+   * from CurrentPage
+   */
+  applyConfig() {
+    if (this._criminal) {
+      this._config = {
+        title: 'CONGRATULATIONS',
+        phrase: 'You have solved the crime in: timer'
+      }
+    }
+  }
+
+  /**
+   * closeApp() method
+   * Closes the application
+   */
+  closeApp() {
+    this.platform.exitApp();
+    /* this.appCtrl.getRootNavById('n4').getAllChildNavs()[0].parent.popToRoot().then(
+      _ => this.navCtrl.pop()
+    ); */
+  }
 }
